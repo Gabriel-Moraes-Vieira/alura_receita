ab63815004f1d4edd81f6f522a101b351e30778f
Gabriel-Moraes-Vieira <gabrieljooj766@gmail.com>
2023-02-21 14:44:43 -0300

    projeto alura_receita

---
+++ pessoas/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Pessoa
+
+class ListandoPessoa(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('nome',)
+    search_fields = ('nome', 'email',)
+    list_per_page = 10
+
+admin.site.register(Pessoa,ListandoPessoa)
